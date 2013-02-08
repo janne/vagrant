e099f71cb5a77a600b7f67a58b26016764cafb46
Jan Andersson <jan.andersson@gmail.com>
2013-02-08 17:23:08 +0100

    Made dotvim use local vimrc and not depend on github.com/janne/dotvim

--- site-cookbooks/dotvim/recipes/default.rb
+++ site-cookbooks/dotvim/recipes/default.rb
@@ -7,19 +7,32 @@
 # All rights reserved - Do Not Redistribute
 #
 
-script "clone_dotvim" do
+%w[.vim .vim/colors].each do |dir|
+  directory "/home/vagrant/#{dir}" do
+    owner "vagrant"
+    mode 0755
+  end
+end
+
+cookbook_file "/home/vagrant/.vimrc" do
+  source "vimrc"
+  owner "vagrant"
+  mode 0644
+end
+
+cookbook_file "/home/vagrant/.vim/colors/desert256.vim" do
+  source "desert256.vim"
+  owner "vagrant"
+  mode 0644
+end
+
+script "clone_vundle" do
   interpreter "bash"
   user "vagrant"
   code <<-EOH
-    cd /home/vagrant
-    if [ -e .vim/.git ]
+    if [ ! -e /home/vagrant/.vim/bundle/vundle ]
     then
-      cd .vim
-      git pull --rebase
-    else
-      git clone git@github.com:janne/dotvim.git .vim
-      git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
-      ln -sf .vim/vimrc .vimrc
+      git clone https://github.com/gmarik/vundle.git /home/vagrant/.vim/bundle/vundle
     fi
     exit 0
   EOH
